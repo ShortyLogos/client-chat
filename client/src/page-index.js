f16cc8dfa972c9452cebd154cbb2f95149c1ac6f
Déric Marchand <dericmarchand@gmail.com>
2022-01-10 22:47:30 -0500

    style login box and add animation footer

--- client/src/page-index.js
+++ client/src/page-index.js
@@ -1,14 +1,14 @@
 import {signin} from './chat-api';
+import Vue from 'vue'
+import Stars from './Stars.vue'
 
 let backgroundImage;
 let posX = 100;
 
 window.addEventListener("load", () => {
-
-    backgroundImage = document.querySelector(".wrapper");
+    backgroundImage = document.querySelector(".wrapper-index");
 
     tick();
-    console.log("Hello");
 
     document.querySelector("form").onsubmit = function () {
         return signin(this);
@@ -17,11 +17,7 @@ window.addEventListener("load", () => {
 
 const tick = () => {
     posX --;
-
     backgroundImage.style.backgroundPosition = posX + "px " + "100%";
-
-    console.log(posX);
-
 
     window.requestAnimationFrame(tick);
 }
