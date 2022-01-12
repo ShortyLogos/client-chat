96f235c64b91dc987578abe2bef920158f43ab02
Déric Marchand <dericmarchand@gmail.com>
2022-01-11 21:49:17 -0500

    start theming register.html + refactoring CSS a bit

--- client/src/page-index.js
+++ client/src/page-index.js
@@ -9,34 +9,6 @@ let body;
 let bodyBgImagePosX = 100;
 
 window.addEventListener("load", () => {
-    let root = document.querySelector(":root");
-    let rootStyle = getComputedStyle(root);
-
-    let mainColor = rootStyle.getPropertyValue('--color-main');
-    let secondaryColor = rootStyle.getPropertyValue('--color-secondary');
-    let paleColor = rootStyle.getPropertyValue('--color-pale');
-    let darkColor = rootStyle.getPropertyValue('--color-dark');
-    let accentColor = rootStyle.getPropertyValue('--color-accent');
-
-    let title = document.querySelector(".client-welcome-title");
-
-    const flashingTitle = () => {
-        setTimeout(() => {
-            title.style.textShadow = "0px 4px " + accentColor;
-            title.style.letterSpacing = "2px";
-        }, 90)
-        setTimeout(() => {
-            title.style.letterSpacing = "3px";
-        }, 180)
-        setTimeout(() => {
-            title.style.textShadow = "0px 2px " + accentColor;
-            title.style.letterSpacing = "2px";
-            flashingTitle();
-        }, 270)
-    }
-
-    flashingTitle();
-
     body = document.querySelector("body");
 
     // background-image: url(../img/background/background-night-sky.png);
@@ -50,10 +22,36 @@ window.addEventListener("load", () => {
         mouseX = event.x;
         mouseY = event.y;
     }
+    
+    let root = document.querySelector(":root");
+    let rootStyle = getComputedStyle(root);
+
+    let mainColor = rootStyle.getPropertyValue('--color-main');
+    let secondaryColor = rootStyle.getPropertyValue('--color-secondary');
+    let paleColor = rootStyle.getPropertyValue('--color-pale');
+    let darkColor = rootStyle.getPropertyValue('--color-dark');
+    let accentColor = rootStyle.getPropertyValue('--color-accent');
+
+    let title = document.querySelector(".client-welcome-title");
+
+    const bouncingTitle = () => {
+        setTimeout(() => {
+            title.style.textShadow = "0px 4px " + accentColor;
+            title.style.letterSpacing = "2px";
+        }, 90)
+        setTimeout(() => {
+            title.style.letterSpacing = "3px";
+        }, 180)
+        setTimeout(() => {
+            title.style.textShadow = "0px 2px " + accentColor;
+            title.style.letterSpacing = "2px";
+            bouncingTitle();
+        }, 270)
+    }
    
     spriteList.push(new UFO());
 
-  
+    bouncingTitle();
     tick();
 
     document.querySelector("form").onsubmit = function () {
