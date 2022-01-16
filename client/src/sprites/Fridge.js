e3a2eaffb3eea8160c648dbfa55f0eadd774abbf
Déric Marchand <dericmarchand@gmail.com>
2022-01-16 16:39:39 -0500

    write random loot table for fridge

--- client/src/sprites/Fridge.js
+++ client/src/sprites/Fridge.js
@@ -9,7 +9,7 @@ export default class Fridge  {
         this.landed = false;
         this.dustStarted = false;
         this.opacity = 1;
-        this.loot = "an ice-cream sandwich.";
+        this.loot = this.randomLoot();
    
         if (window.innerWidth> 1200) {
             this.x = -100 + Math.floor(Math.random() * 275);
@@ -100,17 +100,58 @@ export default class Fridge  {
             setTimeout(() => {
                 this.node.style.backgroundImage = "url(./img/sprites/fridge-open3.png)";
                 this.opened = true;
+
+                let sound = new Audio('./sounds/loot.wav');
+                sound.play();
+
                 let text = "You found " + this.loot;
-                this.popUpBox = new PopUpBox(this.node, text, 12, 1.1, null, 240);
+                this.popUpBox = new PopUpBox(this.node, text, 8, 2, 500, 270);
 
                 // (new PopUpBox(gift, text, 12, 1.1, 1500, 245))
 
                 setTimeout(() => {
                     this.fadeOut = true;
-                }, 3000)
+                }, 2500)
             }, animationSpeed += animationSpeed)
 
         }
+    }
+
+    randomLoot() {
+        let lootTable = [
+            " an ice-cream sandwich.",
+            " a resolve to pursue your dreams.",
+            " 1$. Lucky you!",
+            " a rumor about you. Is it true?.",
+            " a rotten egg. Huh?",
+            " a delicious pudding.",
+            " a future laugh for grim days.",
+            " the laugh of your best friend.",
+            " brand new socks. Wow!",
+            " a chocolate bar big enough to share it.",
+            " a mysterious key.",
+            " a laser gun.",
+            " a bag of marbles.",
+            " a good feeling about tomorrow.",
+            " a bird feather of your favorite color.",
+            " a stinking fart.",
+            " a baseball bat.",
+            " a heartwarming greeting card.",
+            " a new song from your favorite band.",
+            " a coffee-stained newspaper.",
+            " a miniature robot.",
+            " a butterfly.",
+            " a friendly but elusive spider.",
+            " orange juice.",
+            " the most recent password for a private club.",
+            " John. Hello John.",
+            " a way to communicate with turtles.",
+            " an interdimensional portal to a tiny planet."
+        ]
+
+        let roll = Math.floor(Math.random() * lootTable.length);
+
+        return lootTable[roll];
     }
 }
 
